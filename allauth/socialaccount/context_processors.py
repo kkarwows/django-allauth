1881085f7a9225c7538bd76c0649e46cb2bdfe6f
kamil <kkarwows@gmail.com>
2014-05-10 00:33:19 +0200

    fix

--- allauth/socialaccount/context_processors.py
+++ allauth/socialaccount/context_processors.py
@@ -1,6 +1,9 @@
+from . import providers
 from allauth.socialaccount.models import SocialApp
 
 
 def socialaccount(request):
-    ctx = {'providers': SocialApp.objects.values_list('provider', flat=True)}
+    all_providers = providers.registry.get_list()
+    configured = SocialApp.objects.values_list('provider', flat=True)
+    ctx = {'providers':  filter(lambda x: x.id in configured, all_providers)}
     return dict(socialaccount=ctx)
